1061b63ecdd0c45f6efc0ac7165b27e1d40e481a
nuj75 <anujan.kandasamy@gmail.com>
2025-06-21 16:46:46 -0400

    stuff

---
+++ main.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+import google.generativeai as genai
+import os
+
+
+
+url = "https://www.nytimes.com/section/business?page=10"
+
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
+articles = doc.find_all(class_="css-1l4spti")
+
+article_information = ""
+for article in articles:
+    article_information += ("(" + article.find_all('h3')[0].text + "," + article.find_all('p')[0].text + ")" + "||")
+
+
+genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
+
+model = genai.GenerativeModel('gemini-2.5-flash')
+
+prompt = "The following contains tuples of titles and descriptions of articles. What stocks (along with their tickers) are the most impacted by these news" + article_information
+response = model.generate_content(prompt)
+
+print(response.text)
