0ae9d22d7e44e2ed5223628a43fe2e315b7d8745
eweisberg <71267834+classTheTester@users.noreply.github.com>
2025-06-21 17:14:42 -0400

    added endpoint

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, jsonify, send_from_directory
+from main import summarizeNews
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return "/api/news"
+
+
+@app.route('/api/news', methods=['GET'])
+def summarizetext():
+    #index = request.args.get('index')
+    return summarizeNews()
+
+
+
+
+if __name__ == '__main__':
+    print(f"Access the app at: http://localhost:8000")
+    app.run(host='0.0.0.0', port=8000, debug=True)
